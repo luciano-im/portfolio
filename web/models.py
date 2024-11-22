0e1df562137fd5cdd9ece392bc6e2a4ab445f5b7
Luciano Muñoz <luchisds@gmail.com>
2024-11-22 01:08:04 -0300

    🗃️ Add post type field to Post model

--- web/models.py
+++ web/models.py
@@ -8,6 +8,11 @@ class Post(models.Model):
         ('published', 'Published'),
     ]
 
+    POST_TYPE_CHOICES = [
+        ('blog', 'Blog'),
+        ('thought', 'Thought'),
+    ]
+
     title = models.CharField(max_length=200, verbose_name='Título')
     slug = models.SlugField(max_length=200, unique=True)
     content = MartorField(verbose_name='Contenido')
@@ -17,6 +22,7 @@ class Post(models.Model):
     published_at = models.DateTimeField(null=True, blank=True, verbose_name='Fecha de Publicación')
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft', verbose_name='Estado')
     featured_image = models.ImageField(upload_to='posts/', null=True, blank=True, verbose_name='Imágen Destacada')
+    post_type = models.CharField(max_length=10, choices=POST_TYPE_CHOICES, default='blog', verbose_name='Tipo de Post')
 
     def __str__(self):
         return self.title
