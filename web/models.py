50de3ea767db9b8630a3ecc826b9f1dba39dbe89
Luciano Muñoz <luchisds@gmail.com>
2024-11-24 10:59:17 -0300

    🗃️ Create model Project

--- web/models.py
+++ web/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 from martor.models import MartorField
 from taggit.managers import TaggableManager
+from tinymce.models import HTMLField
 
 
 class Post(models.Model):
@@ -32,3 +33,21 @@ class Post(models.Model):
 
     class Meta:
         ordering = ['-created_at']
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=200, verbose_name=_('Nombre del Proyecto'))
+    active = models.BooleanField(default=True, verbose_name=_('Activo'))
+    overview = HTMLField(verbose_name=_('Descripción General'))
+    year = models.DateField(blank=True, null=True, verbose_name=_('Año'))
+    repo = models.URLField(null=True, blank=True, verbose_name=_('URL del Repositorio'), help_text=_('URL de GitHub, GitLab, BitBucket, etc.'))
+    url = models.URLField(null=True, blank=True, verbose_name=_('URL del Proyecto'))
+    intro = HTMLField(verbose_name=_('Introducción'))
+    goals = HTMLField(null=True, blank=True, verbose_name=_('Objetivos'))
+    challenges = HTMLField(null=True, blank=True, verbose_name=_('Desafíos y Dificultades'))
+    solutions = HTMLField(null=True, blank=True, verbose_name=_('Soluciones Implementadas'))
+    impact = HTMLField(null=True, blank=True, verbose_name=_('Impactos del Proyecto'))
+    conclusion = HTMLField(null=True, blank=True, verbose_name=_('Conclusión'))
+
+    def __str__(self):
+        return self.name
