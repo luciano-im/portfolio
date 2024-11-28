fe2dad5d4e96c05f584b24e52f1618dc573f5ea6
Luciano Muñoz <luchisds@gmail.com>
2024-11-27 23:47:33 -0300

    👔 Change home page to fetch posts from database

--- web/views.py
+++ web/views.py
@@ -19,6 +19,11 @@ from web.models import Post
 class HomeView(TemplateView):
     template_name = 'home.html'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['posts'] = Post.objects.filter(status='published', post_type='blog').order_by('-created_at')[:3]
+        return context
+
 
 class BlogView(ListView):
     template_name = 'blog.html'
