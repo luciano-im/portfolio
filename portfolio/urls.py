d417707e96e32735674331c9933a20e4d278e72d
Luciano Muñoz <luchisds@gmail.com>
2025-05-23 00:44:00 -0300

    🌐  Add language prefix in URL

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.conf.urls.i18n import i18n_patterns
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
@@ -14,6 +15,9 @@ urlpatterns = [
     path('api/uploader/', markdown_uploader, name='markdown_uploader_page'),
     path('tinymce/', include('tinymce.urls')),
     path('filer/', include('filer.urls')),
+]
+
+urlpatterns += i18n_patterns(
     path('', HomeView.as_view(), name='home'),
     path('about', AboutView.as_view(), name='about'),
     path('blog', BlogView.as_view(), name='blog'),
@@ -22,7 +26,8 @@ urlpatterns = [
     path('thoughts/<slug:slug>', ThoughtView.as_view(), name='thought'),
     path('projects', ProjectsView.as_view(), name='projects'),
     path('projects/<slug:slug>', ProjectView.as_view(), name='project'),
-]
+    prefix_default_language=False
+)
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
